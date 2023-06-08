ad06301d8520a21aad0915fa4681e235a4178bda
DemonAngeX2 <111583033+DemonAngeX2@users.noreply.github.com>
2023-06-08 10:00:02 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,92 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const path = require('path');
-const {Sequelize}= require("sequelize");
+const { Sequelize } = require('sequelize');
+const dialogData = require('./dialog.json');
 
-
-app.use(express.json())
+app.use(express.json());
 
 // Swagger
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Dialog API',
-            description: 'Dialog API Information',
-            contact: {
-                name: 'Developer Name',
-            },
-            servers: ['http://localhost:3000'],
-        },
+  swaggerDefinition: {
+    info: {
+      title: 'Dialog API',
+      description: 'Dialog API Information',
+      contact: {
+        name: 'Developer Name',
+      },
+      servers: ['http://localhost:3000'],
     },
-    apis: ['./routes/v1/*.js'],
+  },
+  apis: ['./routes/v1/*.js'],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-app.use('/api/v1', require('./routes/v1'))
-
-/*app.get('/', middleware, (req, res) => {
-    res.send('Hello')
-})*/
+app.use('/api/v1', require('./routes/v1'));
 
 app.get('*', (req, res) => {
-    //res.status(404).json({ message: 'Not found'}) bonne pratique
-    res.sendFile(__dirname + "/view/404.html")
-})
-
-app.get('/allchats', (req, res) => {
-    res.status(200).json({
-        data:[{question: "Comment ca va ?", answer: "Bien et toi ?"},
-        {question: "Quel est ton âge ?", answer: "Je suis une IA donc je n'ai pas d'âge"},
-        {question: "Quel temps fait-il à Boulogne ?", answer: "Il fait 22°C"}]
-    })
-})
-
-app.get('/createchat', (req, res) => {
-    res.status(200).json({
-        message:"created"
-    })
-})
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'db/database.sqlite'
+  res.sendFile(path.join(__dirname, 'view', '404.html'));
 });
 
-(async function connect(){
-    try {
+app.get('/allchats', (req, res) => {
+  res.status(200).json(dialogData);
+});
+
+app.get('/createchat', (req, res) => {
+  res.status(200).json({
+    message: 'created',
+  });
+});
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'db/database.sqlite',
+});
+
+(async function connect() {
+  try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
-})()
-
-/*var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "sql7.freesqldatabase.com",
-  user: "sql7623774",
-  password: "4f3lyce9bS"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+})();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})*/
-
-
-
-/*function middleware(req, res, next){
-    console.log('coucou')
-    next()
-}*/
+  console.log(`Example app listening on port ${port}`);
+});
